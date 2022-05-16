57b836d14ec4b134ae3e4cfcfc5c646b59913c31
pemix09 <pemix09@onet.pl>
2022-05-16 14:32:09 +0200

    Added some cleaning and it works

--- WebApp/ClientApp/src/app/app.module.ts
+++ WebApp/ClientApp/src/app/app.module.ts
@@ -7,28 +7,27 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { StudentsComponent } from './Students/students/students.component';
+import { StudentCreateComponent } from './Students/student-create/student-create.component';
+import { StudentUpdateComponent } from './Students/student-update/student-update.component';
+import { StudentListComponent } from './Students/student-list/student-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    StudentsComponent
+    StudentsComponent,
+    StudentCreateComponent,
+    StudentUpdateComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+      { path: '', component: HomeComponent, pathMatch: 'full' }])
   ],
   providers: [],
   bootstrap: [AppComponent]
