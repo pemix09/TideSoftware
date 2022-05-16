b89e02bd028ed9ccb616b9e3d014b660e551e9eb
pemix09 <pemix09@onet.pl>
2022-05-16 12:31:48 +0200

    Created CRUD for students

--- WebApp/Controllers/StudentController.cs
+++ WebApp/Controllers/StudentController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Database;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -8,36 +10,42 @@ namespace WebApp.Controllers
     [Route("[controller]/[Action]")]
     public class StudentController : ControllerBase
     {
+        private AppDbContext context;
+        public StudentController(AppDbContext _context) => context = _context;
+        
         [HttpGet]
         public IEnumerable<Student> GetAll()
         {
-            IEnumerable<Student> students = new List<Student>();
-            return students;
+            return context.Students.ToList();
         }
 
         [HttpGet]
         public Student Get(string Id)
         {
-            Student student = new Student();
+            Student student = context.Students.Find(Id);
             return student;
         }
 
         [HttpPost]
         public void Create(Student student)
         {
-            
+            context.Students.Add(student);
+            context.SaveChanges();
         }
 
         [HttpDelete]
         public void Delete(string Id)
         {
-            
+            Student student = context.Students.Find(Id);
+            context.Students.Remove(student);
+            context.SaveChanges();
         }
 
         [HttpPut]
         public void Update(Student student)
         {
-            //some database operations to perform
+            context.Students.Update(student);
+            context.SaveChanges();
         }
     }
 }
