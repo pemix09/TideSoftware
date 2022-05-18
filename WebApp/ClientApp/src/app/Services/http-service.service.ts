6675435eff874891b2cfe7d7ca1ff554f929dc5e
pemix09 <pemix09@onet.pl>
2022-05-18 13:06:36 +0200

    Completed httpService

--- WebApp/ClientApp/src/app/Services/http-service.service.ts
+++ WebApp/ClientApp/src/app/Services/http-service.service.ts
@@ -35,4 +35,22 @@ export class HttpServiceService {
     this.Http.delete(this.BaseUrl+ this.endPointName+'/Delete/'+StudentId)
       .subscribe(() => this.status = 'Student Deleted successfully!');
   }
+
+  public CreateStudent(student: Student)
+  {
+    this.Http.post(this.BaseUrl+this.endPointName+'/Create/', student)
+      .subscribe(() => this.status = 'Student created successfully');
+  }
+
+  public UpdateStudent(student: Student)
+  {
+    this.Http.put(this.BaseUrl+this.endPointName+'/Update/',student)
+      .subscribe(() => this.status = 'Student Updated successfully');
+  }
+
+  public GetStudentById(studentId: number)
+  {
+    this.Http.get(this.BaseUrl+this.endPointName+'/Get/'+studentId)
+      .subscribe(() => this.status = 'Student get successfully');
+  }
 }
